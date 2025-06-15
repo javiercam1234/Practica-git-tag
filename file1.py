f1d4df8e4e5fec4569b4c9c3a4c407b691e44646
javiercam1234 <javiercam1234@hotmail.com>
2025-06-15 14:57:29 -0600

    ya con el cambio pop

--- file1.py
+++ file1.py
@@ -1,4 +1,7 @@
 print('primer comentario')
 console.log('segundo comentario')
  
- 
+print('tercer comentario')
+console.log('cuarto comentario')
+print('quinto comentario')
+console.log('sexto comentario')

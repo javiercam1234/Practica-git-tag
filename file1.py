ce6f704d4dd61973422c8e045f5e3484df0e6e1f
javiercam1234 <javiercam1234@hotmail.com>
2025-06-15 20:17:55 -0600

    ww

---
+++ file1.py
@@ -0,0 +1,14 @@
+print('primer comentario')
+console.log('segundo comentario')
+ 
+print('tercer comentario')
+console.log('cuarto comentario')
+print('quinto comentario')
+ 
+console.log('sexto comentario') 
+ 
+console.log('sexto comentario')
+
+lo cambie arriba
+ 
+Voy a subirlo ya lo subi jejejejeje haber quq pasa esto esta en la rama 2
